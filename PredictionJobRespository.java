eeb1a3b0ab682d78d0f19e5109d083881f248120
Sinchana0328 <mssinchana23@gmail.com>
2026-09-08 17:12:54 +0530

    Remove duplicate backend files

--- PredictionJobRespository.java
+++
@@ -1,7 +0,0 @@
-package com.oceanembed.backend.repository;
-
-import com.oceanembed.backend.entity.PredictionJob;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PredictionJobRepository extends JpaRepository<PredictionJob, Long> {
-}
--- PredictionResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oceanembed.backend.repository;
-
-import com.oceanembed.backend.entity.PredictionResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PredictionResultRepository extends JpaRepository<PredictionResult, Long> {
-}
